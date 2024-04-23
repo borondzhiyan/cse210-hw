b3af24ba3035a261f66604ea265fe629493aed49
borondzhiyan <borondzhiyan@mail.ru>
2024-04-23 13:47:42 +0300

    Finished Prep 1

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,18 @@
 using System;
+using System.Drawing;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+        string school = "BYU";
+        Console. WriteLine($"I am at {school}.");
+
+        Console.WriteLine("what's your favorite color? ");
+        string color = Console.ReadLine();
+        Console.WriteLine($"your color is {color}");
     }
+
+        
 }
