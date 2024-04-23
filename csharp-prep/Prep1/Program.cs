672245f360bf6d59d164afe58238fd34f34df5cc
borondzhiyan <borondzhiyan@mail.ru>
2024-04-23 14:01:35 +0300

    add a comment

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
+        // Ask the user name
         Console.Write("What is your first name? ");
         string fname = Console.ReadLine();
         Console.Write("What is your last name?? ");
