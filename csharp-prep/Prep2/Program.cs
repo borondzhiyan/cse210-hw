126187537abd0dad262151e7a74d2a8b01f3d273
borondzhiyan <borondzhiyan@mail.ru>
2024-04-24 19:39:49 +0300

    prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,43 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Tell me your score ");
+        string valueFromUser=Console.ReadLine ();
+        int percentage = int.Parse(valueFromUser);
+        string letter = "";
+        if (percentage >=90 )
+        {
+            letter = "A";
+        }
+        else if (percentage >=80)
+        {
+            letter = "B"; 
+        }
+        else if (percentage >=70)
+        {
+            letter = "C";
+        }
+        else if (percentage >=70)
+        {
+            letter = "D";
+        }
+        else if (percentage >=60)
+        {
+            letter = "E";
+        }
+        else
+        {
+            letter="F";
+        }
+        Console.WriteLine($"Your score is {letter} .");
+
+        if (percentage>=70)
+        {
+            Console.WriteLine("You passed the course ");
+        }
+        else
+        {
+            Console.Write("You didn't pass the exam ");
+        }
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,13 +5,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
-        string school = "BYU";
-        Console. WriteLine($"I am at {school}.");
+        // Console.WriteLine("Hello Sandbox World!");
+        // string school = "BYU";
+        // Console. WriteLine($"I am at {school}.");
 
-        Console.WriteLine("what's your favorite color? ");
-        string color = Console.ReadLine();
-        Console.WriteLine($"your color is {color}");
+        // Console.WriteLine("what's your favorite color? ");
+        // string color = Console.ReadLine();
+        // Console.WriteLine($"your color is {color}");
+        Console.Write("Enter the number: ");
+        string valueFromUser = Console.ReadLine ();
+
+        int x=int.Parse (valueFromUser);
+        int y=3;
+
+        if (x>y)
+        {
+            Console.WriteLine("Greater");
+        }
+        else if (x<y)
+        {
+            Console.WriteLine("Less");
+        }
+        else
+        {
+            Console.WriteLine("equal");
+        }
     }
 
         
