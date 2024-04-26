ab7b59801573cdb60e76ea6e00c902b08b419074
borondzhiyan <borondzhiyan@mail.ru>
2024-04-26 18:44:39 +0300

    new

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -8,8 +8,6 @@ class Program
         DisplayWelcome();
         string userName= PromptUserName();
         int numberInt=PromptUserNumber();
-        Console.WriteLine($"Please enter your name: {userName}");
-        Console.WriteLine($"Please enter your favorite number: {numberInt}");
         Console.WriteLine($"{userName}, the square of your number is : {numberInt*numberInt}");
         
     }
@@ -20,13 +18,13 @@ class Program
 
     static string PromptUserName ()
     {
-        Console.WriteLine ("Please enter your name: ");
+        Console.Write ("Please enter your name: ");
         string userName=Console.ReadLine ();
         return userName;
     }
     static int PromptUserNumber ()
     {
-        Console.WriteLine ("Please enter your favorite number: ");
+        Console.Write ("Please enter your favorite number: ");
         string userNumber=Console.ReadLine();
         int numberInt = int.Parse(userNumber);
         return numberInt;
