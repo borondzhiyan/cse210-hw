c117193d046cdb9f72a9910cccbda5bf19033e91
borondzhiyan <borondzhiyan@mail.ru>
2024-05-02 20:22:56 +0300

    new

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,14 @@
+using System;
+
+
+public class Job
+{
+    public string _jobTitle;
+    public string _Company;
+    public int _startYear;
+    public int _endYear;
+    public void Display()
+    { 
+        Console.WriteLine($"{_jobTitle} ({_Company}) {_startYear}-{_endYear}");
+    }
+}   
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._Company = "Microsoft";
+        job1._startYear= 2019;
+        job1._endYear=2022;
+        // Console.WriteLine(job1._Company);
+        // Console.WriteLine(job1._jobTitle);
+        job1.Display();
+
+        Job job2 = new Job();
+        job2._jobTitle="Project manager";
+        job2._Company="Apple";
+        job2._startYear=2022;
+        job2._endYear = 2023;
+        job2.Display();
+
+        Resume resume1 = new Resume();
+        resume1._jobs=new List<Job>();
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+        resume1._name = "Allison Rose";
+        resume1.Display();
+
     }
 }
